c1d8d7b898b93826a730e15852a753f3a8ef0b2e
Muhammad <mja3u@mtmail.mtsu.edu>
2019-11-06 14:37:10 -0600

    Modularized the settings

--- settings.py
+++ settings.py
@@ -1,12 +1,5 @@
 import os
 
-#creator_name = 'Xpherion'
-#mods_folder = os.path.expanduser(
-#    os.path.join('~', 'Documents', 'Electronic Arts', 'The Sims 4', 'Mods')
-#)
-
-#game_folder = os.path.join('D:', os.sep, 'Origin', 'The Sims 4')
-
 class Settings():
     def __init__(self):
         self.creator_name = ""
